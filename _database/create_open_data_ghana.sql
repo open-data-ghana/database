32d2b57742d35fef825a86d6d1aa3e09101db45f
jeff <jeff.ohene10@gmail.com>
2024-06-01 15:34:09 +0200

    tables regions, constituencies and population added

--- _database/create_open_data_ghana.sql
+++ _database/create_open_data_ghana.sql
@@ -1,5 +1,20 @@
--- This code creates the database. Run this entire script only once unless there's a major update.
--- WARNING: Running this script multiple times will create duplicate databases. Proceed with caution!
+/* 
+This code creates the database. Run this entire script only once unless there's a major update. 
+Run the first query to check if database exists. If it returns false, then you can run the entire script.
+*/
+
+-- Check if the database exists
+SELECT
+    CASE 
+        WHEN EXISTS (
+            SELECT 1
+            FROM pg_database
+            WHERE datname = 'open_data_ghana'
+        )
+        THEN 'true'
+        ELSE 'false'
+    END AS db_available;
+
 
 -- Create the database 
 CREATE DATABASE open_data_ghana
